8073226d718e55b4adb763a4f5a639dc77836411
adrianvinuelas <adrian.vinuelas@gmail.com>
2015-05-18 17:19:18 +0200

    finalizado y limpiado el código y añadido funcionalidad firefox os

--- adivina.js
+++ adivina.js
@@ -4,63 +4,48 @@ var namegame = "";
 var latitud = 0;
 var longitud = 0;
 var nombrelugar = "";
-var hayintervalo = false;
-var intervalo;
+var hayintervalo = false;//para saber si hay intervalo de mostrar las fotos
+var intervalo;//intervalo en el que se muestran las fotos
 var numimagenes = 0;
-var inicializarmapa = false;
-var inicializarbotones = true;
-var inicializarbotones1 = true;
-var inicializarbotones2 = true;
-var inicializarbotones3 = true;
-var map;
-var numberOfEntries = 0;
-var juegos;
-var numjuegosanadidos= 0;
-var llamadago = false;
-var anadejuego = false;
-var anadir = true;
-var go=false;
+var inicializarmapa = false;//inicializar el mapa cada vez que hay que mostrarlo
+var inicializarbotones = true;//inicializar botones
+var inicializarbotones1 = true;//inicializar botones
+var inicializarbotones2 = true;//inicializar botones
+var inicializarbotones3 = true;//inicializar botones
+var map; 
+var numberOfEntries = 0; //longitud del historial
+var juegos;//string con los juegos a añadir al html
+var numjuegosanadidos= 0;//para saber cuantos juegos juegados he añadido para mostrar 
+var go=false;//para saber si voy a volver atrás en el historial
 var ibapor= 0;//para recuperar en el historial por donde iba
 var numanterior = 0;
 
-window.onpopstate = function(event) {
-	//alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
-	//console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+window.onpopstate = function(event) {//evento que salta cada vez que se hace un go o back
 	if(!go){
 		if(event.state != null){
-			if(JSON.stringify(event.state.namegame) != undefined){
-				//console.log("añado juego " + JSON.stringify(event.state.namegame) + ", nivel : "+ JSON.stringify(event.state.difstring))
-				//console.log("numgo = " + numgo)
-				//console.log("numjuegosanadidos = " + numjuegosanadidos)
+			if(JSON.stringify(event.state.namegame) != undefined){//si hay namegame es una partida
 				if(numjuegosanadidos < 5){
-					//console.log("añado juego a la lista");
 					juegos+= "<li onclick='volverjugar("+numgo+");empezarpartida("+JSON.stringify(event.state.namegame)+
 							","+JSON.stringify(event.state.difstring) +");'>juego : " 
 							+ JSON.stringify(event.state.namegame) + ", nivel : " 
 							+JSON.stringify(event.state.difstring)+", puntos: "
 							+JSON.stringify(event.state.puntos)+"</li>";
 					numjuegosanadidos++;
-				}
-				
+				}				
 			}
 		}
-	}
-		
+	}		
 }
 
 function juegosjugados(){
-	console.log("entro a juegos jugados");
 	juegos ="<p><ul>";
 	numjuegosanadidos = 0;
 	numanterior = 0;
 	numberOfEntries = window.history.length;
-	//console.log("numberOfEntries juegojugados = " + numberOfEntries);
 	if(numberOfEntries>1){
 		for(i=1; i<numberOfEntries;i++){
-			numgo= -(i);
-			//console.log("history.back -" + i);
+			numgo= -(i);//variable para saber cuantos go hay que hacer si me llaman a ese juego
    			window.history.back();
-   			//console.log("numero de entradas despues de back = " +window.history.length)
    		}
    		window.history.go(numberOfEntries-1);
 	}
@@ -69,45 +54,30 @@ function juegosjugados(){
 }
 
 function volverjugar(num){
-	//para saber si me dan un go(2)por ejemplo,cual era la posicion anterior
-	console.log("entro a volver ajugar")
 	numjuegosanadidos = 0;
-	//le paso el num del que tengo que hacer go ->go(-3)
-	if(num>0){//si me llaman despues de retroceder con un go hacia adelante
-		//console.log("go hacia adelante despues de haber vuelto a atrás!!!!!!!!!!!!!!")
+	if(num>0){//si me llaman después de retroceder, con un go hacia adelante
 		window.history.go(ibapor);//vuelvo al pincipio del historial para recalcular
-		//console.log("historylengh antes de cambiar = " + window.history.length)
-		//console.log("num antes de cambiar = " + num);
-		//console.log("numanterior antes de cambiar = " + numanterior);
 		num = numanterior + num;//(-(4) + 2) por ejemplo
-		//console.log("num despues de cambiar = " + num);
-	}else if(num<0 && numanterior !=0 ){//si me llaman despues de retroceder,para que retroceda(recalculo)
+	}else if(num<0 && numanterior !=0 ){//si me llaman después de retroceder,para que retroceda(recalculo)
 		window.history.go(ibapor);//vuelvo al principio del historial para recalcular
 		num = numanterior + num;//(-4 + (-1)) por ejemplo
 	}
 	go = false;//para añadir partidas si las hay
 	numberOfEntries = window.history.length;
 	juegos ="<p><ul>";
-	//console.log("numberOfEntries = " + numberOfEntries);
 	if(numberOfEntries>1){
 		for(i=1; i<numberOfEntries;i++){
 			if(i< -(num)){
-				numgo = -(num) - i;// -(-3)-1 = 2 por ejemplo
+				numgo = -(num) - i;// -(-3)-1 = 2 (go)
 			}else if(i> -(num)){
-				numgo = -(i+num); //-(5+(-3)) = -2
-				//console.log("entro a numgo = " + numgo);
+				numgo = -(i+num); //-(5+(-3)) = -2 (back)
 			}else if(i = -(num)){
 				numgo = 0;
 			}
    			window.history.back();
-   			//console.log("history.back " + -(i));
-   			//console.log("numero de entradas despues de back = " +window.history.length)
    		}
    		go=true;//variable para indicar que no estoy en la ultima entrada si esta a true
-   		//console.log("numero total = " + numberOfEntries);
-   		//console.log("numero que retroceso = " +  num);
    		ibapor =  -num; //-(-3) para hacer un go(3) despues de jugar
-   		//console.log("numero que tengo que hacer go despues = " + ibapor);
    		window.history.go(numberOfEntries-1);//vuelvo al principio
    		window.history.go(num);//hago el go que corresponde
 	}
@@ -117,7 +87,6 @@ function volverjugar(num){
 }
 
 function inicio(){
-	//console.log("pulsado el boton inicio");
 	$("#selec-juego").hide();
 	$("#myCarousel").show();
 	$("#partida").hide();
@@ -128,10 +97,9 @@ function inicio(){
 	$("#ayuda").hide();
 	if(hayintervalo){
 		hayintervalo=false;
-		window.clearInterval(intervalo)
-		//console.log("cerrado el intervalo")
+		window.clearInterval(intervalo);
 	}
-	window.history.pushState({}, "ADIVINAgame", "inicio");
+	window.history.pushState({}, "IADIVINA", "inicio");
 	juegosjugados();
 	
 
@@ -148,7 +116,11 @@ function ayuda(){
 	$("#myCarousel").hide();
 	$("#juegosjugados1").show();
 	$("#ayuda").show();
-	window.history.pushState({}, "ADIVINAgame", "ayuda");
+	if(hayintervalo){
+		hayintervalo=false;
+		window.clearInterval(intervalo);
+	}
+	window.history.pushState({}, "AADIVINA", "ayuda");
 	juegosjugados();
 }
 
@@ -160,20 +132,16 @@ function selecdif(nombrejuego){
 	}
 	$("#juegos").hide();
 	$("#juegoselec").html("HAS SELECCIONADO JUGAR A : " + namegame);
-	//console.log("el nombre del juego es : " + namegame);
 	$("#dificultad").show();
 	if(inicializarbotones1){
 		inicializarbotones1 = false;
 		$("#botonfacil").click(function(){
-			//console.log("val() = " + $("#botonfacil").val());
 			dificultad($("#botonfacil").val());
 		});
 		$("#botonmedio").click(function(){
-				//console.log("val() = " + $("#botonmedio").val());
 				dificultad($("#botonmedio").val());
 		});
 		$("#botondificil").click(function(){
-				//console.log("val() = " + $("#botondificil").val());
 				dificultad($("#botondificil").val());
 		});
 	}
@@ -181,7 +149,6 @@ function selecdif(nombrejuego){
 }
 
 function dificultad(valor){
-	//console.log("VALOR = " + valor);
 	if(valor=="1"){
 		dif=5;
 		difstring = "FÁCIL";
@@ -201,12 +168,9 @@ function dificultad(valor){
 		$("#botondeshacer").click(jugar);
 	}	
 	$("#barraprogreso").css( "width", "100%" );
-
-	//console.log("el valor de la dificultad es : " + valor);
 }
 
 function jugar(){
-	//console.log("pulsado el boton jugar");
 	$("#juegoselec").html("");
 	$("#difselec").html("");
 	$("#selec-juego").show();
@@ -230,9 +194,12 @@ function jugar(){
 		});
 	}
 	$("#barraprogreso").css( "width", "0%" );
-	window.history.pushState({}, "ADIVINAgame", "jugar");
+	window.history.pushState({}, "JADIVINA", "jugar");
+	if(hayintervalo){
+		hayintervalo=false;
+		window.clearInterval(intervalo);
+	}
 	if(go){
-		//console.log("vuelvo a por donde iba")
 		window.history.go(ibapor);
 		go=false;
 	}
@@ -240,11 +207,9 @@ function jugar(){
 	
 }
 function onMapClick(e) {
-	//console.log("pinchado mapa");
 	if(hayintervalo){
 		hayintervalo=false;
-		window.clearInterval(intervalo)
-		//console.log("cerrado el intervalo")
+		window.clearInterval(intervalo);
 	}
 	var R = 6371.0;//Radio de la Tierra para calcular la distancia
 	var diflat = ((latitud - e.latlng.lat) * Math.PI) /180;
@@ -252,20 +217,15 @@ function onMapClick(e) {
 	//a = sin²(Δlat/2) + cos(lat1) · cos(lat2) · sin²(Δlong/2)
 	var a = Math.sin(diflat/2)*Math.sin(diflat/2) + Math.cos((e.latlng.lat*Math.PI)/180)
 			* Math.cos((latitud * Math.PI)/180) * Math.sin(diflng/2) *Math.sin(diflng/2);
-	//console.log("a = " + a);
 	//c = 2 · atan2(√a, √(1−a))
 	var c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-	//console.log("c = " + c);
 	var distancia = parseInt(R * c);
-	//console.log("distancia entre los dos puntos es = " + distancia);
 	var puntuacion ="";
 	var result = 0;
 	if(distancia <= 200){
-		//console.log("entra a ganar");
 		puntuacion = "HAS GANADO";
 	}else{
 		result = 1;
-		//console.log("entra a perder")
 		puntuacion = "PERDISTE, VUELVE A INTENTARLO";
 	}
 	if(numimagenes==0){
@@ -290,21 +250,13 @@ function onMapClick(e) {
 	$("#puntuacion").show();
 	$("#juegosjugados1").hide();
 	$('#fotos').html("");
-	anadejuego = true;
 	if(go){
-		//console.log("vuelvo a por donde iba")
 		window.history.go(ibapor);
 		go=false;
 	}
-	if(hayintervalo){
-		hayintervalo=false;
-		window.clearInterval(intervalo)
-		//console.log("cerrado el intervalo")
-	}
-	//crear el objeto para que se inicialice a null
 	var obj = {namegame: namegame, difstring: difstring, puntos: puntos}
-	window.history.pushState(obj, "ADIVINAgame", namegame+difstring);
-}	//acordarme de que cambie el guardar partida para que funcione con el servidor
+	window.history.pushState(obj, "PADIVINA", namegame+difstring);
+}	
 
 function empezarpartida(njug,ndif){
 	if(ndif != undefined){
@@ -313,14 +265,13 @@ function empezarpartida(njug,ndif){
 	}
 	if(hayintervalo){
 		hayintervalo=false;
-		window.clearInterval(intervalo)
+		window.clearInterval(intervalo);
 		$('#fotos').html("");
-		//console.log("cerrado el intervalo")
 	}
 	if(!go){//si esta a true es que he vuelto a atrás en el historial
-		window.history.pushState({}, "ADIVINAgame", namegame+difstring);
+		window.history.pushState({}, "PADIVINA", namegame+difstring);
 	}
-	//console.log("QUE EMPIECE LA PARTIDA!")
+	
 	numimagenes = 0;
 	$("#selec-juego").hide();
 	$("#myCarousel").hide();
@@ -330,12 +281,11 @@ function empezarpartida(njug,ndif){
 				", BUENA SUERTE!</H1>");
 	$("#partida").show();
 	
-	//history.pushState(data, event.target.textContent, event.target.href);
 	if(inicializarbotones3){
 		inicializarbotones3=false;
 		$("#cancelgame").click(jugar);
 	}
-	if(inicializarmapa){//para volver a cargar el mapa,la primera vez no se mete
+	if(inicializarmapa){//para volver a cargar el mapa,la primera vez no me meto
 		map.remove();
 	}
 	
@@ -354,12 +304,10 @@ function empezarpartida(njug,ndif){
 	
 
 	$.getJSON("http://adrianvinuelas.github.io/X-Nav-Practica-Adivina/juegos/"+namegame+".json",function(data){
-		//console.log("estoy dentro del json");
 		var enigma = data.features[Math.round(Math.random() * 10)];
 		latitud = enigma.coordinates[0];
 		longitud = enigma.coordinates[1];
 		nombrelugar = enigma.properties.name;
-		console.log("el sitio a adivinar es -> " + enigma.properties.name )
 		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
 			{
 	          tags: enigma.properties.name,//nombre de la ciudad o pais del que se buscan las fotos
@@ -367,14 +315,11 @@ function empezarpartida(njug,ndif){
 	          format: "json"
         	},	function(data){
         		var num=0;
-        		//console.log("data.items.length = " + data.items.length);
         		intervalo = setInterval(function(){
         			hayintervalo = true;
-        			//console.log("se ejecuta el intervalo");
         			var imagen = "<img src="+ data.items[num].media.m
         				+" style='width: 100%; height: 360px;'>";
         				$('#fotos').html(imagen);
-        				console.log("num = " + num);
         				if(num == data.items.length - 1){//si es la ultima foto vuelvo a la primera
         					num = 0;
         				}else{//si no sigo cogiendo la siguiente foto
@@ -388,7 +333,6 @@ function empezarpartida(njug,ndif){
 }
 
 jQuery(document).ready(function() {
-	//console.log("dom cargado modificado 1");
 	$("#partida").hide();
 	$("#selec-juego").hide();
 	$("#resultado").hide();
